7cbc372b462c07ffc3df67e59a58105028d75d85
Sherzod Axadov <sherzodaxadov69@gmail.com>
2023-05-24 03:03:10 +0500

    build: file structure

---
+++ app.vue
@@ -0,0 +1,8 @@
+<template>
+  <div>
+    <FormRegister/>
+  </div>
+</template>
+<script setup lang="ts">
+import FormRegister from "~/components/FormRegister.vue";
+</script>
---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [],
+  theme: {
+    extend: {
+      colors:{
+        white:{
+          DEFAULT:'#fff',
+          light:'#f6f6f6',
+          bold:'#333'
+        }
+      }
+    },
+  },
+  plugins: [],
+}
+
