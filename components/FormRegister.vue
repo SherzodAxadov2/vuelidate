f59823b00bdd999d0aa8c640a8b5ee1af18e5461
Sherzod Axadov <sherzodaxadov69@gmail.com>
2023-05-24 06:38:31 +0500

    form validation completed

--- components/FormRegister.vue
+++ components/FormRegister.vue
@@ -1,22 +1,31 @@
 <template>
-  <main class="min-h-screen bg-[#1a1a1a]">
-    <section class="container mx-auto px-5 py-24 text-gray-400">
-      <form
-          class="bg-opacity-50 mx-auto mt-10 flex w-full flex-col rounded-lg bg-[#242424] p-8 shadow-lg md:mt-0 md:w-1/2 lg:w-2/6"
+  <main class="min-h-screen bg-[#1a1a1a]" @submit.prevent="submitForm">
+    <section class="container mx-auto px-5 py-14 text-gray-400">
+      <form ref="form"
+          class="bg-opacity-50  mx-auto flex w-full flex-col rounded-lg bg-[#242424] p-8 shadow-lg md:mt-0 md:w-1/2 lg:w-2/6"
       >
         <h2 class="mb-5 text-lg font-medium text-[#aac8e4]">Register</h2>
 
         <!-- Email input -->
-        <div class="relative mb-4">
+        <div class="relative mb-5">
           <label for="email" class="text-sm leading-7 text-gray-400">Email</label>
           <div class="relative">
             <input
+                v-model="formData.email"
                 type="text"
                 id="email"
                 name="email"
                 placeholder="e.g. example@email.com"
                 class="bg-opacity-20 w-full rounded border border-gray-600 bg-transparent py-1 px-3 text-base leading-8 text-gray-100 outline-none transition-colors duration-200 ease-in-out placeholder:text-gray-500 focus:border-blue-500 focus:bg-transparent focus:ring-2 focus:ring-transparent"
+                @change="v$.email.$touch()"
+                :class="{'border-red-500 focus:border-red-500' : v$.email.$error,
+                'border-[#42d392] ': !v$.email.$invalid
+                }"
             />
+            <Icon v-if="!v$.email.$invalid || v$.email.$error" class="absolute right-2 h-full text-xl text-green-500" :class="{ 'text-green-500': !v$.email.$invalid, 'text-yellow-500': v$.email.$error }"
+                  :name="`heroicons-solid:${!v$.email.$error ? 'check-circle' : 'exclamation'}`"
+            />
+<!--            <span class="absolute left-0 -bottom-5 text-[12px] text-red-500">{{v$.email.$error}}</span>-->
           </div>
         </div>
 
@@ -25,10 +34,18 @@
           <label for="password" class="text-sm leading-7 text-gray-400">Password</label>
           <div class="relative">
             <input
+                v-model="formData.password"
                 type="password"
                 id="password"
                 name="password"
                 class="bg-opacity-20 w-full rounded border border-gray-600 bg-transparent py-1 px-3 text-base leading-8 text-gray-100 outline-none transition-colors duration-200 ease-in-out focus:border-blue-500 focus:bg-transparent focus:ring-2 focus:ring-transparent"
+                @change="v$.password.$touch()"
+                :class="{'border-red-500 focus:border-red-500' : v$.password.$error,
+                'border-[#42d392] ': !v$.password.$invalid
+                }"
+            />
+            <Icon v-if="!v$.password.$invalid || v$.password.$error" class="absolute right-2 h-full text-xl text-green-500" :class="{ 'text-green-500': !v$.password.$invalid, 'text-yellow-500': v$.password.$error }"
+                  :name="`heroicons-solid:${!v$.password.$error ? 'check-circle' : 'exclamation'}`"
             />
           </div>
         </div>
@@ -40,10 +57,38 @@
           >
           <div class="relative">
             <input
+                v-model="formData.confirmPassword"
                 type="password"
                 id="confirmPassword"
                 name="confirmPassword"
                 class="bg-opacity-20 w-full rounded border border-gray-600 bg-transparent py-1 px-3 text-base leading-8 text-gray-100 outline-none transition-colors duration-200 ease-in-out focus:border-blue-500 focus:bg-transparent focus:ring-2 focus:ring-transparent"
+                @change="v$.confirmPassword.$touch()"
+                :class="{'border-red-500 focus:border-red-500':v$.confirmPassword.$error,
+                'border-[#42d392]': !v$.confirmPassword.$invalid}"
+            />
+            <Icon v-if="!v$.confirmPassword.$invalid || v$.confirmPassword.$error" class="absolute right-2 h-full text-xl text-green-500" :class="{ 'text-green-500': !v$.confirmPassword.$invalid, 'text-yellow-500': v$.confirmPassword.$error }"
+                  :name="`heroicons-solid:${!v$.confirmPassword.$error ? 'check-circle' : 'exclamation'}`"
+            />
+          </div>
+        </div>
+
+        <!-- Code button -->
+        <div class="relative mb-4">
+          <label for="code" class="text-sm leading-7 text-gray-400"
+          >Enter your code</label
+          >
+          <div class="relative">
+            <input
+                v-model="formData.code"
+                type="number"
+                id="code"
+                name="code"
+                class="bg-opacity-20 w-full rounded border border-gray-600 bg-transparent py-1 px-3 text-base leading-8 text-gray-100 outline-none transition-colors duration-200 ease-in-out focus:border-blue-500 focus:bg-transparent focus:ring-2 focus:ring-transparent"
+                @change="v$.code.$touch()"
+                :class="{'border-red-500 focus:border-red-500': v$.code.$error,
+                'border-[#42d392]': !v$.code.$invalid}"
+            />
+            <Icon v-if="v$.code.$error || !v$.code.$invalid" class="absolute right-2 h-full text-xl text-green-500" :class="{'text-green-500': !v$.code.$invalid,'text-yellow-500':v$.code.$error}" :name="`heroicons-solid:${v$.code.$error?'exclamation':'check-circle'}`"
             />
           </div>
         </div>
@@ -58,5 +103,51 @@
   </main>
 </template>
 
-<script setup lang="ts">
+<script setup>
+import {required, email, maxLength, minLength, sameAs, helpers} from "@vuelidate/validators";
+import useVuelidate from "@vuelidate/core";
+
+const formData = reactive({
+  email:'',
+  password:'',
+  confirmPassword:'',
+  code:'',
+})
+const form =ref()
+const rules = computed(()=>{
+  return{
+    email: {
+      required: helpers.withMessage('The email field is required', required),
+      email: helpers.withMessage('Invalid email format!', email)
+    },
+    password:{
+      required: helpers.withMessage('The password field is required', required),
+      minLength: minLength(6)
+    },
+    confirmPassword:{
+      required: helpers.withMessage('The password comfirmation field is required', required),
+      sameAs: helpers.withMessage('Password don\'t match',sameAs(formData.password))
+    },
+    code:{
+      required: helpers.withMessage('The code field is required', required),
+      maxLength: maxLength(6),
+      minLength:minLength(6)
+    }
+  }
+})
+
+const v$ = useVuelidate(rules, formData)
+
+const submitForm = ()=>{
+  console.log(formData.value)
+  v$.value.$validate();
+  if(!v$.value.$error){
+    formData.email = ''
+    formData.password = ''
+    formData.confirmPassword = ''
+    formData.code = ''
+    v$.value.$reset()
+  }
+}
+
 </script>
